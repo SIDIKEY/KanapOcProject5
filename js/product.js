61525d1e9db92803937ae23c2fd9dd91ddb64623
SIDIKEY <sidikikeitadixinne@gmail.com>
2022-08-16 02:28:36 +0200

    get the ID for each and fetching datas

--- js/product.js
+++ js/product.js
@@ -1,3 +1,12 @@
-const product = window.location.search.split("?").join("");
-console.log(product);
-let productData = [];
+const str = window.location;
+const url = new URL(str);
+const id = url.searchParams.get("id");
+const host = "http://localhost:3000/api/products";
+const objectURL = host + "api/products/" + id;
+
+const fetchProduct =async () => {
+    await fetch(`http://localhost:3000/api/products/${id}`)
+    .then ((res) => res.json())
+    .then ((promise) => {console.log(promise);})
+}
+fetchProduct();
