901d7e3009540e87e1bf2c961d675cd43efaac93
SIDIKEY <sidikikeitadixinne@gmail.com>
2022-08-17 20:10:07 +0200

    add price, descript, loop colors

--- js/product.js
+++ js/product.js
@@ -1,12 +1,46 @@
+
+
 const str = window.location;
 const url = new URL(str);
 const id = url.searchParams.get("id");
-const host = "http://localhost:3000/api/products";
+const host = "http://localhost:3000/";
 const objectURL = host + "api/products/" + id;
 
+let productData = [];
+
+
+
 const fetchProduct =async () => {
-    await fetch(`http://localhost:3000/api/products/${id}`)
+    await fetch(objectURL)
     .then ((res) => res.json())
-    .then ((promise) => {console.log(promise);})
+    .then ((promise) => {productData = promise
+    console.log(productData); })   
+};
+
+
+const productDisplay = async () =>{
+    await fetchProduct();
+    document.getElementById("item__img").innerHTML = `
+    <img src="${productData.imageUrl}" alt="${productData.altTxt}">
+    `;
+    document.getElementById("title").innerHTML = `
+    ${productData.name}
+    `;
+    document.getElementById("price").innerHTML = `
+    ${productData.price}
+    `;
+    document.getElementById("description").innerHTML = `
+    ${productData.description}
+    `;
+    
+    
+    let select = document.getElementById("colors");
+    console.log(select);
+    console.log(productData.colors);
+
+    for (i = 0; i < productData.colors.length; i++) {
+        select.innerHTML += `<option value="${productData.colors[i]}">${productData.colors[i]}</option>`;
+      };
+    console.log(productData.colors.length);
 }
-fetchProduct();
+productDisplay();
